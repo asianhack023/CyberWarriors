d054a8c311623b97e328a17e3c2dc6d46ce50256
heisenbergsujan <suzanpande@gmail.com>
2024-08-11 08:46:57 +0545

    last commit

--- script.js
+++ script.js
@@ -354,108 +354,6 @@ document.querySelector("#course_main").addEventListener("click", function () {
   );
 });
 
-document.querySelector(".btn-login").addEventListener("click", function () {
-  document.querySelector(".main-content").innerHTML = "";
-  document.querySelector(".main-content").insertAdjacentHTML(
-    "afterbegin",
-    ` <div class="px-40 flex flex-1 justify-center py-5">
-      <div
-        class="ring-2 ring-blue-500 shadow shadow-blue-100 layout-content-container flex flex-col w-[512px] max-w-[512px] py-5 max-w-[960px] flex-1"
-      >
-        <h1
-          class="text-white text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 text-left pb-3 pt-5"
-        >
-          Log in to Cybersecurity Academy
-        </h1>
-        <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
-          <label class="flex flex-col min-w-40 flex-1">
-            <input
-              placeholder="Email or username"
-              class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-white focus:outline-0 focus:ring-0 border-none bg-[#223749] focus:border-none h-14 placeholder:text-[#90b0cb] p-4 text-base font-normal leading-normal"
-              value=""
-            />
-          </label>
-        </div>
-        <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
-          <label class="flex flex-col min-w-40 flex-1">
-            <div class="flex w-full flex-1 items-stretch rounded-xl">
-              <input
-                placeholder="Password"
-                class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-white focus:outline-0 focus:ring-0 border-none bg-[#223749] focus:border-none h-14 placeholder:text-[#90b0cb] p-4 rounded-r-none border-r-0 pr-2 text-base font-normal leading-normal"
-                value=""
-              />
-              <div
-                class="text-[#90b0cb] flex border-none bg-[#223749] items-center justify-center pr-4 rounded-r-xl border-l-0"
-                data-icon="Eye"
-                data-size="24px"
-                data-weight="regular"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24px"
-                  height="24px"
-                  fill="currentColor"
-                  viewBox="0 0 256 256"
-                >
-                  <path
-                    d="M247.31,124.76c-.35-.79-8.82-19.58-27.65-38.41C194.57,61.26,162.88,48,128,48S61.43,61.26,36.34,86.35C17.51,105.18,9,124,8.69,124.76a8,8,0,0,0,0,6.5c.35.79,8.82,19.57,27.65,38.4C61.43,194.74,93.12,208,128,208s66.57-13.26,91.66-38.34c18.83-18.83,27.3-37.61,27.65-38.4A8,8,0,0,0,247.31,124.76ZM128,192c-30.78,0-57.67-11.19-79.93-33.25A133.47,133.47,0,0,1,25,128,133.33,133.33,0,0,1,48.07,97.25C70.33,75.19,97.22,64,128,64s57.67,11.19,79.93,33.25A133.46,133.46,0,0,1,231.05,128C223.84,141.46,192.43,192,128,192Zm0-112a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160Z"
-                  ></path>
-                </svg>
-              </div>
-            </div>
-          </label>
-        </div>
-        <div class="px-4">
-          <label class="flex gap-x-3 py-3 flex-row">
-            <input
-              type="checkbox"
-              class="h-5 w-5 rounded border-[#314f68] border-2 bg-transparent text-[#2094f3] checked:bg-[#2094f3] checked:border-[#2094f3] checked:bg-[image:--checkbox-tick-svg] focus:ring-0 focus:ring-offset-0 focus:border-[#314f68] focus:outline-none"
-            />
-            <p class="text-white text-base font-normal leading-normal">
-              Remember me
-            </p>
-          </label>
-        </div>
-        <div class="flex px-4 py-3">
-          <button
-            class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 flex-1 bg-[#2094f3] text-white text-sm font-bold leading-normal tracking-[0.015em]"
-          >
-            <span class="truncate">Log in</span>
-          </button>
-        </div>
-        <p
-          class="text-[#90b0cb] text-sm font-normal leading-normal pb-3 pt-1 px-4 text-center"
-        >
-          or
-        </p>
-        <div class="flex justify-stretch">
-          <div class="flex flex-1 gap-3 flex-wrap px-4 py-3 justify-start">
-            <button
-              class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#223749] text-white text-sm font-bold leading-normal tracking-[0.015em]"
-            >
-              <span class="truncate">Continue with Google</span>
-            </button>
-            <button
-              class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#223749] text-white text-sm font-bold leading-normal tracking-[0.015em]"
-            >
-              <span class="truncate">Continue with GitHub</span>
-            </button>
-          </div>
-        </div>
-        <p
-          class="text-[#90b0cb] text-sm font-normal leading-normal pb-3 pt-1 px-4 text-center underline"
-        >
-          Forgot your password?
-        </p>
-        <p
-          class="text-[#90b0cb] text-sm font-normal leading-normal pb-3 pt-1 px-4 text-center underline"
-        >
-          New to Cybersecurity Academy? Sign up here
-        </p>
-      </div>
-    </div>`
-  );
-});
 document.querySelector(".btn-signup").addEventListener("click", function () {
   console.log();
   document.querySelector(".main-content").innerHTML = "";
@@ -553,5 +451,109 @@ document.querySelector(".btn-signup").addEventListener("click", function () {
             </p>
           </div>
         </div>`
+  );
+});
+
+document.querySelector(".btn-login").addEventListener("click", function () {
+  document.querySelector(".main-content").innerHTML = "";
+  document.querySelector(".main-content").insertAdjacentHTML(
+    "afterbegin",
+    ` <div class="px-40 flex flex-1 justify-center py-5">
+      <div
+        class="ring-2 ring-blue-500 shadow shadow-blue-100 layout-content-container flex flex-col w-[512px] max-w-[512px] py-5 max-w-[960px] flex-1 main-content-login"
+      >
+        <h1
+          class="text-white text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 text-left pb-3 pt-5"
+        >
+          Log in to Cybersecurity Academy
+        </h1>
+        <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+          <label class="flex flex-col min-w-40 flex-1">
+            <input
+              placeholder="Email or username"
+              class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-white focus:outline-0 focus:ring-0 border-none bg-[#223749] focus:border-none h-14 placeholder:text-[#90b0cb] p-4 text-base font-normal leading-normal"
+              value=""
+            />
+          </label>
+        </div>
+        <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+          <label class="flex flex-col min-w-40 flex-1">
+            <div class="flex w-full flex-1 items-stretch rounded-xl">
+              <input
+                placeholder="Password"
+                class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-white focus:outline-0 focus:ring-0 border-none bg-[#223749] focus:border-none h-14 placeholder:text-[#90b0cb] p-4 rounded-r-none border-r-0 pr-2 text-base font-normal leading-normal"
+                value=""
+              />
+              <div
+                class="text-[#90b0cb] flex border-none bg-[#223749] items-center justify-center pr-4 rounded-r-xl border-l-0"
+                data-icon="Eye"
+                data-size="24px"
+                data-weight="regular"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24px"
+                  height="24px"
+                  fill="currentColor"
+                  viewBox="0 0 256 256"
+                >
+                  <path
+                    d="M247.31,124.76c-.35-.79-8.82-19.58-27.65-38.41C194.57,61.26,162.88,48,128,48S61.43,61.26,36.34,86.35C17.51,105.18,9,124,8.69,124.76a8,8,0,0,0,0,6.5c.35.79,8.82,19.57,27.65,38.4C61.43,194.74,93.12,208,128,208s66.57-13.26,91.66-38.34c18.83-18.83,27.3-37.61,27.65-38.4A8,8,0,0,0,247.31,124.76ZM128,192c-30.78,0-57.67-11.19-79.93-33.25A133.47,133.47,0,0,1,25,128,133.33,133.33,0,0,1,48.07,97.25C70.33,75.19,97.22,64,128,64s57.67,11.19,79.93,33.25A133.46,133.46,0,0,1,231.05,128C223.84,141.46,192.43,192,128,192Zm0-112a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160Z"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+          </label>
+        </div>
+        <div class="px-4">
+          <label class="flex gap-x-3 py-3 flex-row">
+            <input
+              type="checkbox"
+              class="h-5 w-5 rounded border-[#314f68] border-2 bg-transparent text-[#2094f3] checked:bg-[#2094f3] checked:border-[#2094f3] checked:bg-[image:--checkbox-tick-svg] focus:ring-0 focus:ring-offset-0 focus:border-[#314f68] focus:outline-none"
+            />
+            <p class="text-white text-base font-normal leading-normal">
+              Remember me
+            </p>
+          </label>
+        </div>
+        <div class="flex px-4 py-3">
+          <button
+            class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 flex-1 bg-[#2094f3] text-white text-sm font-bold leading-normal tracking-[0.015em]"
+            id="btn-submit-login"
+          >
+            <span class="truncate">Log in</span>
+          </button>
+        </div>
+        <p
+          class="text-[#90b0cb] text-sm font-normal leading-normal pb-3 pt-1 px-4 text-center"
+        >
+          or
+        </p>
+        <div class="flex justify-stretch">
+          <div class="flex flex-1 gap-3 flex-wrap px-4 py-3 justify-start">
+            <button
+              class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#223749] text-white text-sm font-bold leading-normal tracking-[0.015em]"
+            >
+              <span class="truncate">Continue with Google</span>
+            </button>
+            <button
+              class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#223749] text-white text-sm font-bold leading-normal tracking-[0.015em]"
+            >
+              <span class="truncate">Continue with GitHub</span>
+            </button>
+          </div>
+        </div>
+        <p
+          class="text-[#90b0cb] text-sm font-normal leading-normal pb-3 pt-1 px-4 text-center underline"
+        >
+          Forgot your password?
+        </p>
+        <p
+          class="text-[#90b0cb] text-sm font-normal leading-normal pb-3 pt-1 px-4 text-center underline"
+        >
+          New to Cybersecurity Academy? Sign up here
+        </p>
+      </div>
+    </div>`
   );
 });
